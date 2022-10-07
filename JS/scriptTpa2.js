d7711680faedd64dac3fa132da363471d70a8b0d
Hafizhabiyyu777 <hafizhfirdaus19@gmail.com>
2022-10-07 20:54:07 +0700

    Merapikan code js

--- JS/scriptTpa2.js
+++ JS/scriptTpa2.js
@@ -3,56 +3,38 @@ let height = document.getElementById("height");
 let weight = document.getElementById("weight");
 let result = document.getElementById("result");
 
-
 calculate.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if(cekangka(weight.value,height.value)) {
-        let bmi;
-        let pesan="";
-        bmi = weight.value / Math.pow(height.value / 100, 2);
-        bmi = bmi.toFixed(1)
-        console.log("Ini bmi : "+bmi);
+  event.preventDefault();
+  if (cekangka(weight.value, height.value)) {
+    let bmi;
+    let pesan = "";
+    bmi = weight.value / Math.pow(height.value / 100, 2);
+    bmi = bmi.toFixed(2);
 
-        if (bmi < 18.5) {
-            pesan="Underweight"
-        } else if (bmi >= 18.5 && bmi <= 24.9) {
-          pesan="Normal weight"
-        } else if (bmi >= 25 && bmi <= 29.9) {
-          pesan="Overweight"
-        } else if(bmi>=30) {
-          pesan="Obesity"
-        }
-        console.log(height.value);
-        console.log(weight.value);
-        console.log(bmi);
-        // console.log(bmi.toFixed(1));
-        result.innerHTML = `Berat anda : ${weight.value} <br>Tinggi anda : ${height.value} <br> Skor BMI anda adalah <B>${bmi}</B> serta termasuk kategori <b>${pesan}</b>`;
-        result.style.color= "black"
-        calculate.reset()
-    }
-    else {
-        result.innerHTML=`<b>Mohon untuk menginputkan angka saja!</b>`
-        result.style.color= "red"
-        calculate.reset()
+    if (bmi < 18.5) {
+      pesan = "Underweight";
+    } else if (bmi >= 18.5 && bmi < 25) {
+      pesan = "Normal weight";
+    } else if (bmi >= 25 && bmi < 30) {
+      pesan = "Overweight";
+    } else if (bmi >= 30) {
+      pesan = "Obesity";
     }
 
-  
- 
-
+    result.innerHTML = `Berat anda : ${weight.value} <br>Tinggi anda : ${height.value} <br> Skor BMI anda adalah <B>${bmi}</B> serta termasuk kategori <b>${pesan}</b>`;
+    result.style.color = "black";
+    calculate.reset();
+  } else {
+    result.innerHTML = `<b>Mohon untuk menginputkan angka saja!</b>`;
+    result.style.color = "red";
+    calculate.reset();
+  }
 });
 
-
-
-
-
-function cekangka (berat,tinggi) {
-    if(isNaN(berat) && isNaN(tinggi)  ) {
-        return false
-        
-    }
-    else {
-        return true
-    }
+function cekangka(berat, tinggi) {
+  if (isNaN(berat) && isNaN(tinggi)) {
+    return false;
+  } else {
+    return true;
+  }
 }
-
-
